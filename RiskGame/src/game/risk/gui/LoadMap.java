e58e9bcce8a879b8c457ad1d7d47dfcfb2df655c
himanshumahajan <himanshumahajan@himanshus-mbp.wireless.concordia.ca>
2017-09-29 17:55:07 -0400

    Fetching countries and displaying on UI

--- RiskGame/src/game/risk/gui/LoadMap.java
+++ RiskGame/src/game/risk/gui/LoadMap.java
@@ -12,34 +12,52 @@ public static void main(String[] args) throws Exception
 {  
 	
 	
-    JFrame f=new JFrame("LOAD MAP GUI");  
+    JFrame f=new JFrame("MAP GUI");  
     
-    JButton b=new JButton("Click Here");  
-    
+    JButton b;
     JLabel l1,l2;  
-    l1=new JLabel("Continents");  
-    l1.setBounds(40,48, 100,30);  
+    JComboBox cb1,cb2;
+    
+    
     
     MapReader mapreader = new MapReader();
     RiskMap riskmap = mapreader.readMap();
     
     riskmap.getContinents();
     
-    JComboBox cb=new JComboBox(riskmap.getContinents().keySet().toArray());    
-    cb.setBounds(40, 50,190,120);    
-    f.add(cb);     
+        
+    
+    b=new JButton("Click Here");  
+    l1=new JLabel("Continents");  
+    l2=new JLabel("Countries"); 
+    cb1=new JComboBox(riskmap.getContinents().keySet().toArray());
+    cb2=new JComboBox(riskmap.getTerritories().keySet().toArray());
+    
+    //b.setBounds(155,110,100,35);  
+    l1.setBounds(40,48, 100,30); 
+    l2.setBounds(250,48, 100,30);
+    cb1.setBounds(40, 50,190,120); 
+    cb2.setBounds(250, 50,190,120); 
+    
+    
+    f.add(cb1);  
+    f.add(cb2);
     f.add(l1);
+    f.add(l2);
+    f.add(b);
     
     
-    b.setBounds(55,110,100,35);  
+    
     b.addActionListener(new ActionListener(){  
     public void actionPerformed(ActionEvent e){  
-            
+            b.setText("hey buddy");
         }  
     });  
     
     f.setSize(500,500);  
     f.setLayout(null);  
     f.setVisible(true);   
+    
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }  
 }  
