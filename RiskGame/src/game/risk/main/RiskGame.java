52f45177bc6032d4434d087fe46463541630d880
Harpreet Singh <hs.harpreet101@gmail.com>
2017-10-13 13:18:41 -0400

    Refactoring code

--- RiskGame/src/game/risk/main/RiskGame.java
+++ RiskGame/src/game/risk/main/RiskGame.java
@@ -36,30 +36,33 @@ import game.risk.util.RiskMap;
 
 public class RiskGame {
 
-	static ArrayList<String> files = null;
-	static String mapSeleted = "";
-	static int computersSeleted = 0;
-	static JFrame jFrame = null;
-	static GameDriver gameDriver = null;
+	private static String mapSeleted = "";
+	private static int computersSeleted = 0;
+	private static JFrame jFrame = null;
+	private static GameDriver gameDriver = null;
 
+	private static String SELECTED_MAP;
+	private static int SELECTED_NO_OF_COMPUTERS;
+	private static int NO_OF_HUMAN = 1;
+	private static int SET_NO_OF_CONTINETS;
+	private static int SET_NO_OF_TERRITORIES;
+	private static int TOTAL_PLAYERS;
 	
 	public RiskGame() {
-		files = new ArrayList<String>();
 		jFrame = new JFrame("RISK");
 		gameDriver = new GameDriver();
 
 	}
 	
-	public Boolean getMapFiles(String s){
-		Boolean mapAvailable = false;
-		File folder = new File(s);
+	public ArrayList<String> getMapFiles(String folderPath){
+		ArrayList<String> files = new ArrayList<String>();
+		File folder = new File(folderPath);
 		File[] listOfFiles = null;
 		if(folder.exists()){
 			listOfFiles = folder.listFiles();
 		}
 		
 		  if(listOfFiles != null){
-			  mapAvailable = true;
 			  for (int i = 0; i < listOfFiles.length; i++) {
 				  if (listOfFiles[i].isFile() && listOfFiles[i].toString().endsWith(".map")) {
 					  files.add(listOfFiles[i].getName().substring(0, listOfFiles[i].getName().length()-4));
@@ -67,37 +70,33 @@ public class RiskGame {
 			  }
 		  }
 		  
-		return mapAvailable;
+		return files;
 	} 
 	
-	public JPanel headerPanel(){
-		
-		return null;
-	}
-	
-	public JPanel footerPanel(){
-		
-		return null;
-	}
-	
-	public void setGamePanels(JPanel middlePanel, String status){
-        JPanel headerPanel = getPanel();
-        JPanel footerPanel = getPanel();
-        
-        headerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        
+	public static JPanel headerPanel(String status){
+		JPanel headerPanel = customHeaderAndFooterPanel();
+		headerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+	        
         headerPanel.setPreferredSize(new Dimension(600, 40));
         headerPanel.add(customJLabel(RiskGameConstants.GAME_NAME, 30, Color.BLACK));
         headerPanel.add(customJLabel(status, 20, Color.BLACK));
+		return headerPanel;
+	}
+	
+	public static JPanel footerPanel(){
+		JPanel footerPanel = customHeaderAndFooterPanel();
+		footerPanel.setPreferredSize(new Dimension(600, 40));
+        footerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		return footerPanel;
+	}
+	
+	public void setGamePanels(JPanel middlePanel, String status){
         
         middlePanel.setLayout(new GridLayout(0,1));
         
-        footerPanel.setPreferredSize(new Dimension(600, 40));
-        footerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        
-        jFrame.getContentPane().add(headerPanel, BorderLayout.NORTH);
+        jFrame.getContentPane().add(headerPanel(status), BorderLayout.NORTH);
         jFrame.getContentPane().add(middlePanel, BorderLayout.CENTER);
-        jFrame.getContentPane().add(footerPanel, BorderLayout.SOUTH);
+        jFrame.getContentPane().add(footerPanel(), BorderLayout.SOUTH);
         jFrame.pack();
 
         jFrame.setSize(600,600);  
@@ -108,8 +107,8 @@ public class RiskGame {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-	public JPanel startPanel(){
-		JPanel middleStartPanel = getPanel();
+	public JPanel startPanel(ArrayList<String> files){
+		JPanel middleStartPanel = customHeaderAndFooterPanel();
 		DefaultListModel<String> tempList = new DefaultListModel<>(); 
 		for(String temp: files){
 			tempList.addElement(temp);  
@@ -124,7 +123,7 @@ public class RiskGame {
 		warning.setVisible(false);
 		middleStartPanel.add(listOfMaps); 
 		
-		JPanel temp = getPanel();
+		JPanel temp = customHeaderAndFooterPanel();
 		temp.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
 		JButton okButton = customJButton(RiskGameConstants.OK, 15);
@@ -138,12 +137,12 @@ public class RiskGame {
 				if(listOfMaps.getSelectedValue()!= null){
 					mapSeleted = listOfMaps.getSelectedValue();
 					
-					RiskGameConstants.SELECTED_MAP = mapSeleted+".map";
+					SELECTED_MAP = mapSeleted+".map";
 					
 					middleStartPanel.removeAll();
 					middleStartPanel.revalidate();
 					middleStartPanel.repaint();
-					middleStartPanel.add(secondPanel(RiskGameConstants.SELECTED_MAP));
+					middleStartPanel.add(secondPanel(SELECTED_MAP));
 				}else{
 					warning.setVisible(true);
 					Timer timer = new Timer(3000, new ActionListener() {
@@ -168,7 +167,7 @@ public class RiskGame {
 	}
 	
 	public JPanel secondPanel(String mapSeleted){
-		JPanel middleSecondPanel = getPanel();
+		JPanel middleSecondPanel = customHeaderAndFooterPanel();
 		
 		middleSecondPanel.add(customJLabel(RiskGameConstants.NO_OF_COMPUTERS, 20, Color.BLACK));
 		
@@ -176,7 +175,7 @@ public class RiskGame {
 	    JComboBox cb=new JComboBox(noOfPlayers);    
 	    middleSecondPanel.add(cb);        
 		
-		JPanel temp = getPanel();
+		JPanel temp = customHeaderAndFooterPanel();
 		temp.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
 		JButton okButton = customJButton(RiskGameConstants.OK, 15);
@@ -190,15 +189,15 @@ public class RiskGame {
 				if(cb.getSelectedItem()!= null){
 					//mapSeleted = listOfMaps.getSelectedValue();
 					computersSeleted = Integer.parseInt((String) cb.getSelectedItem());
-					RiskGameConstants.SELECTED_NO_OF_COMPUTERS = computersSeleted;
+					SELECTED_NO_OF_COMPUTERS = computersSeleted;
 					System.out.println(computersSeleted);
 					middleSecondPanel.removeAll();
 					middleSecondPanel.revalidate();
 					middleSecondPanel.repaint();
 					
-					RiskGameConstants.TOTAL_PLAYERS = RiskGameConstants.SELECTED_NO_OF_COMPUTERS + RiskGameConstants.NO_OF_HUMAN;
+					TOTAL_PLAYERS = SELECTED_NO_OF_COMPUTERS + NO_OF_HUMAN;
 					
-					middleSecondPanel.add(thirdPanel(RiskGameConstants.SELECTED_MAP, RiskGameConstants.TOTAL_PLAYERS));
+					middleSecondPanel.add(thirdPanel(SELECTED_MAP, TOTAL_PLAYERS));
 				}else{
 					Timer timer = new Timer(3000, new ActionListener() {
 			            @Override
@@ -224,13 +223,13 @@ public class RiskGame {
 	
 	public int noOfArmiesIntially(){
 		int temp = 0;
-		if(RiskGameConstants.TOTAL_PLAYERS == 2){
+		if(TOTAL_PLAYERS == 2){
 			temp = 40;
-		} else if(RiskGameConstants.TOTAL_PLAYERS == 3){
+		} else if(TOTAL_PLAYERS == 3){
 			temp = 35;
-		} else if(RiskGameConstants.TOTAL_PLAYERS == 4){
+		} else if(TOTAL_PLAYERS == 4){
 			temp = 30;
-		} else if(RiskGameConstants.TOTAL_PLAYERS == 5){
+		} else if(TOTAL_PLAYERS == 5){
 			temp = 25;
 		}
 		
@@ -249,14 +248,14 @@ public class RiskGame {
 			HashMap<String, String> continents = riskMap.getContinents();
 			HashMap<String, Territory> territories = riskMap.getTerritories();
 			
-			listOfAssignedTerritories = getCountriesList(RiskGameConstants.TOTAL_PLAYERS, 
+			listOfAssignedTerritories = getCountriesList(TOTAL_PLAYERS, 
 					 territories);
 			
-			RiskGameConstants.SET_NO_OF_CONTINETS = continents.size();
-			RiskGameConstants.SET_NO_OF_TERRITORIES = territories.size();
+			SET_NO_OF_CONTINETS = continents.size();
+			SET_NO_OF_TERRITORIES = territories.size();
 			//ArrayList<Territory> Territories = new ArrayList<Territory>();
 			ArrayList<Player> players  = new ArrayList<Player>();
-			for(int i=0;i<=RiskGameConstants.SELECTED_NO_OF_COMPUTERS; i++){
+			for(int i=0;i<=SELECTED_NO_OF_COMPUTERS; i++){
 				
 				if(i ==0){
 					Player human = new Player();
@@ -305,15 +304,17 @@ public class RiskGame {
 
 	public static void main(String[] args) {
 		RiskGame riskGame = new RiskGame();
-		JPanel middlePanel = getPanel();
-		
-		Boolean mapAvailable = riskGame.getMapFiles("Maps");
+		JPanel middlePanel = customHeaderAndFooterPanel();
+		String folderPath = "Maps";
+		ArrayList<String> files = riskGame.getMapFiles(folderPath);
+		Boolean mapAvailable = (files.size()>0)? true: false;
 		
 		if(mapAvailable){
-			middlePanel = riskGame.startPanel();
+			middlePanel = riskGame.startPanel(files);
 			riskGame.setGamePanels(middlePanel, "Map Selection");
 		}else{
 			middlePanel.add(customJLabel("NO MAPS AVAILABLE, Check folder", 20, Color.RED));
+			riskGame.setGamePanels(middlePanel, "Map Selection");
 		}
 		//riskGame.setGamePanels(middlePanel, "Map Selection");
 		
@@ -334,7 +335,7 @@ public class RiskGame {
 		return tempButton;
 	}
 
-	private static JPanel getPanel(){
+	private static JPanel customHeaderAndFooterPanel(){
         JPanel result = new JPanel();
         result.setBorder(new EmptyBorder(50, 10, 10, 10));
         result.setPreferredSize(new Dimension(600, 40));
