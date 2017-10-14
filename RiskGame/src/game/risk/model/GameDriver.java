8d3720b04dd0b0dc219592636c1cdf52462699ed
sjbachher <sjbachher12@gmail.com>
2017-10-14 12:34:11 -0400

    Refactoring of GameDriver.java

--- RiskGame/src/game/risk/model/GameDriver.java
+++ RiskGame/src/game/risk/model/GameDriver.java
@@ -1,6 +1,11 @@
 package game.risk.model;
 
 import java.util.ArrayList;
+/**
+ * A java class which drives the game.
+ * @author Simran
+ *
+ */
 
 public class GameDriver {
 
@@ -10,30 +15,38 @@ public class GameDriver {
 	
 	Player player;
 	ArrayList<Player> listPlayer;
+	// Method to get player
 	public Player getPlayer() {
 		return player;
 	}
+	// Method to set player
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
+	// Method to list all the players
 	public ArrayList<Player> getListPlayer() {
 		return listPlayer;
 	}
+	// Method to save the list of players
 	public void setListPlayer(ArrayList<Player> listPlayer) {
 		this.listPlayer = listPlayer;
 	}
+	//  Method to get the total players
 	public int getTotalPlayer() {
 		return totalPlayer;
 	}
+	//Method to set the total number of players
 	public void setTotalPlayer(int totalPlayer) {
 		this.totalPlayer = totalPlayer;
 	}
+	// Method to get the total territories
 	public int getTotalTerritories() {
 		return totalTerritories;
 	}
 	public void setTotalTerritories(int totalTerritories) {
 		this.totalTerritories = totalTerritories;
 	}
+	// Method to  get the number of territories per player.
 	public int getTerritoriesPerPlayer() {
 		return territoriesPerPlayer;
 	}
