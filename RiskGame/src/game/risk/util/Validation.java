a4f6bb491de8601878610d4245f26d9f0b23d7ec
himanshumahajan <himanshumahajan@himanshus-mbp.wireless.concordia.ca>
2017-10-16 15:57:13 -0400

    Validating that country does not add itself in adjacent country from drop down

--- RiskGame/src/game/risk/util/Validation.java
+++ RiskGame/src/game/risk/util/Validation.java
@@ -38,5 +38,31 @@ public boolean validateAddLink(String countryName , String linkName , String map
 	}
 	return true;
 }
+
+
+public boolean validateAddLinkAddingItself(String countryName , String linkName , String mapFileName)throws Exception
+{
+    String country = countryName;
+    String link = linkName;
+    String mapfile = mapFileName;
+	File inputFile = new File(mapfile);
+	BufferedReader br = new BufferedReader(new FileReader(inputFile));
+	String thisLine = "";
+	while ((thisLine = br.readLine()) != null) {
+		if (thisLine.equalsIgnoreCase("[Territories]")) 
+		{
+			while ((thisLine = br.readLine()) != null && thisLine != "") {
+				String[] columns = thisLine.split(",");
+				if (columns[0].equalsIgnoreCase(link)) 
+				{
+					return false;
+                 }
+	
+}
+		}
+	}
+	return true;
+}
+
 }
 	
