64f0d190c11c3e97c681fe273bffedb88ec53956
sjbachher <sjbachher12@gmail.com>
2017-10-17 18:12:29 -0400

    Refactoring of RiskMap

--- RiskGame/src/game/risk/util/RiskMap.java
+++ RiskGame/src/game/risk/util/RiskMap.java
@@ -4,28 +4,52 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import game.risk.model.Territory;
-
+/**
+ * A class which stores information about the map, continents and continents
+ * @author Team
+ *
+ */
 public class RiskMap {
-	
+	//Declaration
 	HashMap<String, String> map ;
 	HashMap<String, String> continents ;
 	HashMap<String, Territory> territories ;
-	
+	/**
+	 * 
+	 * @param map a hashMap for MAP
+	 * @param continents a map for continents
+	 * @param territories a map for territories
+	 */
 	public RiskMap(HashMap<String, String> map, HashMap<String, String> continents, HashMap<String, Territory> territories) {
 		super();
 		this.map = map;
 		this.continents = continents;
 		this.territories = territories;
 	}
+	/**
+	 * A constructor
+	 */
 	public RiskMap() {
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * Method to get Map and store it into a hashMap
+	 * @return map 
+	 */
 	public HashMap<String, String> getMap() {
 		return map;
 	}
+	/**
+	 * Method to get continents and store it into a hashMap
+	 * @return Continents 
+	 */
 	public HashMap<String, String> getContinents() {
 		return continents;
 	}
+	/**
+	 * Method to get territories and store it into a hashMap
+	 * @return territories
+	 */
 	public HashMap<String, Territory> getTerritories() {
 		return territories;
 	}
