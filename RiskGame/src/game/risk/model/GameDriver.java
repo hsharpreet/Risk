e837f730551f280fbf593b3668d6160346698a6d
sjbachher <sjbachher12@gmail.com>
2017-10-17 01:51:00 -0400

    Update to GameDriver

--- RiskGame/src/game/risk/model/GameDriver.java
+++ RiskGame/src/game/risk/model/GameDriver.java
@@ -3,7 +3,7 @@ package game.risk.model;
 import java.util.ArrayList;
 /**
  * A java class which drives the game.
- * @author Simran
+ * @author Team
  *
  */
 
@@ -15,41 +15,73 @@ public class GameDriver {
 	
 	Player player;
 	ArrayList<Player> listPlayer;
-	// Method to get player
+	/**
+	 * Method to get player
+	 * @return Player 
+	 */
 	public Player getPlayer() {
 		return player;
 	}
-	// Method to set player
+	/**
+	 * Method to set Players
+	 * @param player
+	 */
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
-	// Method to list all the players
+	/**
+	 * Method to get the list of players
+	 * @return ListPlayers The list of players
+	 */
 	public ArrayList<Player> getListPlayer() {
 		return listPlayer;
 	}
-	// Method to save the list of players
+	/**
+	 * Method to set the list of players
+	 * @param listPlayer The list of players
+	 */
 	public void setListPlayer(ArrayList<Player> listPlayer) {
 		this.listPlayer = listPlayer;
 	}
-	//  Method to get the total players
+	/**
+	 * Method to get the total number of players
+	 * @return totalPlayer The total  number players
+	 */
 	public int getTotalPlayer() {
 		return totalPlayer;
 	}
-	//Method to set the total number of players
+	/**
+	 * Method to set the total number of players
+	 * @return totalPlayer The total  number players
+	 */
 	public void setTotalPlayer(int totalPlayer) {
 		this.totalPlayer = totalPlayer;
 	}
-	// Method to get the total territories
+	/**
+	 * Method to get the total territories
+	 * @return totalTerritories
+	 */
 	public int getTotalTerritories() {
 		return totalTerritories;
 	}
+	/**
+	 * Method to set total territoriess
+	 * @param totalTerritories
+	 */
 	public void setTotalTerritories(int totalTerritories) {
 		this.totalTerritories = totalTerritories;
 	}
-	// Method to  get the number of territories per player.
+	/**
+	 * Method to get the territories per player
+	 * @return territoriesPerPlayer.
+	 */
 	public int getTerritoriesPerPlayer() {
 		return territoriesPerPlayer;
 	}
+	/**
+	 * Method to set the territories per player.
+	 * @param territoriesPerPlayer
+	 */
 	public void setTerritoriesPerPlayer(int territoriesPerPlayer) {
 		this.territoriesPerPlayer = territoriesPerPlayer;
 	}
