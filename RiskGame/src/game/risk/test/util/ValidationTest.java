85e4366d4e252d88bba4ebdc411e737d3d4ed73d
Mandeep1151 <mandeep1151@gmail.com>
2017-10-19 15:55:30 -0400

    Adding new Tests for MapWriter

--- RiskGame/src/game/risk/test/util/ValidationTest.java
+++ RiskGame/src/game/risk/test/util/ValidationTest.java
@@ -32,5 +32,63 @@ public class ValidationTest {
 		assertTrue(result);
 	}
 	
+	@Test
+	public void testvalidateAddLinkAddingItselfFalse() throws Exception {
+		String thisLine ="Alaska,30,46,North America,Northwest Territory,Alberta,Kamchatka";
+		String country= "Alaska";
+		String neighbouringCountry="Alaska";
+		
+		Validation validateMapWriter = new Validation();
+		boolean result = validateMapWriter.validateAddLinkAddingItself(country, neighbouringCountry, thisLine);
+		
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testvalidateAddLinkAddingItselfTrue() throws Exception {
+		String thisLine ="Alaska,30,46,North America,Northwest Territory,Alberta,Kamchatka";
+		String country= "Alaska";
+		String neighbouringCountry="India";
+		
+		Validation validateMapWriter = new Validation();
+		boolean result = validateMapWriter.validateAddLinkAddingItself(country, neighbouringCountry, thisLine);
+		
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testvalidateLinkToDeleteTrue() throws Exception {
+		
+		String countryLink= "Argentina";
+		String mapFile="World_TerritoryWithOneNeighbour.map";
+		
+		Validation validateMapWriter = new Validation();
+		boolean result = validateMapWriter.validateLinkToDelete(countryLink, mapFile);
+		
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testCheckTerritoriesBeforeDeletingContinentFalse() throws Exception {
+		
+		String mapFile="WorldDeleteInvalid.map";
+		
+		Validation validateMapWriter = new Validation();
+		boolean result = validateMapWriter.checkTerritoriesBeforeDeletingContinent("hello", mapFile);
+		
+		assertFalse(result);
+	}
+
+	@Test
+	public void testCheckAdjacentTerritoryLinkBeforeDeleteFalse() throws Exception {
+		
+		String mapFile="WorldDeleteInvalid2.map";
+		
+		Validation validateMapWriter = new Validation();
+		boolean result = validateMapWriter.checkAdjacentTerritoryLinkBeforeDelete("test1", mapFile);
+		
+		assertFalse(result);
+	}
+	
 	
 }
