a982245ed75106c8f23bfe8adc6356e9ede27d8f
sjbachher <sjbachher12@gmail.com>
2017-10-19 15:58:11 -0400

    java doc added to territory,java

--- RiskGame/src/game/risk/model/Territory.java
+++ RiskGame/src/game/risk/model/Territory.java
@@ -3,7 +3,7 @@ package game.risk.model;
 import java.util.List;
 /**
  * A java class to store information about territories
- * @author Simran
+ * @author Team
  *
  */
 public class Territory {
@@ -15,67 +15,103 @@ public class Territory {
 	private List<String> neighbouringTerritories;
 
 	
-	// Method to get name of the territory
+	// 
+	/**
+	 * Method to get name of the territory
+	 * @return name the name of the territory 
+	 */
 	public String getName() {
 		return name;
 	}
 
 
-	// Method to set name of the territory
+	// 
+	/**
+	 * Method to set name of the territory in the class attribute
+	 * @param name the name of the territory
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 
-	// Method to get x coordinates of the territory
+	/**
+	 * Method to get the x coordinate of the territory
+	 * @return coordinateX the x coordinate of the territory
+	 */
 	public String getCoordinateX() {
 		return coordinateX;
 	}  
 
 
-	// Method to set x coordinates of the territory
+	
+	/**
+	 * Method to set the x coordinate in the class attribute
+	 * @param coordinateX the x coordinate of the territory
+	 */
 	public void setCoordinateX(String coordinateX) {
 		this.coordinateX = coordinateX;
 	}
 
 
-	// Method to get y coordinates of the territory
+	/**
+	 * Method to get the y coordinate of the territory
+	 * @return coordinateY the y coordinate 
+	 */
 	public String getCoordinateY() {
 		return coordinateY;
 	}
 
 
-	// Method to set y coordinates of the territory
+	/**
+	 * Method to set the y coordinate in the class attribute
+	 * @param coordinateY the y coordinate
+	 */
 	public void setCoordinateY(String coordinateY) {
 		this.coordinateY = coordinateY;
 	}
 
 
-	// Method to get the continent
+	/**
+	 * Method to get the y coordinate of the territory
+	 * @return continent the continent of the territory
+	 */
 	public String getContinent() {
 		return continent;
 	}
 
 
-	// Method to set the continent
+	/**
+	 * Method to set the continent in the class attribute
+	 * @param continent the continent
+	 */
 	public void setContinent(String continent) {
 		this.continent = continent;
 	}
 
 
-	// Method to get neighboring territories
+	/**
+	 * Method to get the neighboring territories
+	 * @return neighboringTerritories the neighboring territories
+	 */
 	public List<String> getNeighbouringTerritories() {
 		return neighbouringTerritories;
 	}
 
 
-	// Method to set the neighboring territories
+	/**
+	 * Method to set the neighboring territories in the class attribute
+	 * @param neighbouringTerritories
+	 */
 	public void setNeighbouringTerritories(List<String> neighbouringTerritories) {
 		this.neighbouringTerritories = neighbouringTerritories;
 	}
 
 
-	//Method to append information to the file.
+	/**
+	 * Method to append the territory information in the file
+	 * 
+	 */
 	@Override
 	public String toString() {
 		StringBuffer territoryInformation = new StringBuffer();
