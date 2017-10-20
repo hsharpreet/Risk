a5b02acb72b53116bf65295a310187201499b4bf
sjbachher <sjbachher12@gmail.com>
2017-10-19 21:33:38 -0400

    Change to code and rafactoring

--- RiskGame/src/game/risk/util/RiskMap.java
+++ RiskGame/src/game/risk/util/RiskMap.java
@@ -3,65 +3,83 @@ package game.risk.util;
 import java.util.HashMap;
 
 import game.risk.model.Territory;
+
 /**
  * A class which stores information about the map, continents and continents
+ * 
  * @author Team
  *
  */
 public class RiskMap {
-	//Declaration
-	HashMap<String, String> map ;
-	HashMap<String, String> continents ;
-	HashMap<String, Territory> territories ;
+	// Declaration
+	HashMap<String, String> map;
+	HashMap<String, String> continents;
+	HashMap<String, Territory> territories;
+
 	/**
 	 * A constructor
-	 * @param map a hashMap for MAP
-	 * @param continents a hashMap for continents
-	 * @param territories a hashMap for territories
+	 * 
+	 * @param map
+	 *            a hashMap for MAP
+	 * @param continents
+	 *            a hashMap for continents
+	 * @param territories
+	 *            a hashMap for territories
 	 */
-	public RiskMap(HashMap<String, String> map, HashMap<String, String> continents, HashMap<String, Territory> territories) {
+	public RiskMap(HashMap<String, String> map, HashMap<String, String> continents,
+			HashMap<String, Territory> territories) {
 		super();
 		this.map = map;
 		this.continents = continents;
 		this.territories = territories;
 	}
+
 	/**
 	 * A constructor
-	 * @param continents a hashMap of continents
-	 * @param territories a hashMap of territories
+	 * 
+	 * @param continents
+	 *            a hashMap of continents
+	 * @param territories
+	 *            a hashMap of territories
 	 */
 	public RiskMap(HashMap<String, String> continents, HashMap<String, Territory> territories) {
 		super();
 		this.continents = continents;
 		this.territories = territories;
 	}
+
 	/**
 	 * A constructor
 	 */
 	public RiskMap() {
 		// TODO Auto-generated constructor stub
 	}
+
 	/**
 	 * Method to get Map and store it into a hashMap
-	 * @return map 
+	 * 
+	 * @return map
 	 */
 	public HashMap<String, String> getMap() {
 		return map;
 	}
+
 	/**
 	 * Method to get continents and store it into a hashMap
-	 * @return Continents 
+	 * 
+	 * @return Continents
 	 */
 	public HashMap<String, String> getContinents() {
 		return continents;
 	}
+
 	/**
 	 * Method to get territories and store it into a hashMap
+	 * 
 	 * @return territories
 	 */
 	public HashMap<String, Territory> getTerritories() {
 		return territories;
 	}
-	
-	
+
 }
--- RiskGame/src/game/risk/util/ValidateMapReader.java
+++ RiskGame/src/game/risk/util/ValidateMapReader.java
@@ -3,21 +3,26 @@ package game.risk.util;
 import java.util.HashMap;
 
 import game.risk.model.Territory;
+
 /**
- * Class to validate map reader 
+ * Class to validate map reader
+ * 
  * @author Team
  *
  */
 public class ValidateMapReader {
-/**
- *
- * Method to check if the map is valid
- * @param continents a hashMap of continents
- * @param territories a hashMap of territory
- * @return true if the map is valid
- * @return false if the map is not valid
- * 
- */ 
+	/**
+	 *
+	 * Method to check if the map is valid
+	 * 
+	 * @param continents
+	 *            a hashMap of continents
+	 * @param territories
+	 *            a hashMap of territory
+	 * @return true if the map is valid
+	 * @return false if the map is not valid
+	 * 
+	 */
 	public boolean isMapValid(HashMap<String, String> continents, HashMap<String, Territory> territories) {
 		if (checkCountriesExistInValidContinent(continents, territories) && checkNeighbouringCountriesExist(territories)
 				&& checkNeighbouringCountriesSymmetric(territories)) {
@@ -26,13 +31,17 @@ public class ValidateMapReader {
 		return false;
 
 	}
-/**
- * Method to check that countries exist in valid continents
- * @param continents a hashMap of the continents
- * @param territories a hashMap of the territory
- * @return false if countries do not exist in valid continent
- * @return true if countries exist in valid continent
- */
+
+	/**
+	 * Method to check that countries exist in valid continents
+	 * 
+	 * @param continents
+	 *            a hashMap of the continents
+	 * @param territories
+	 *            a hashMap of the territory
+	 * @return false if countries do not exist in valid continent
+	 * @return true if countries exist in valid continent
+	 */
 	public boolean checkCountriesExistInValidContinent(HashMap<String, String> continents,
 			HashMap<String, Territory> territories) {
 		for (Territory t : territories.values()) {
@@ -42,12 +51,15 @@ public class ValidateMapReader {
 		}
 		return true;
 	}
-/**
- * Method to check if the neighboring countries exist
- * @param territories a hashMap of territories 
- * @return false if neighboring countries do not exist
- * @return true if neighboring countries exist
- */
+
+	/**
+	 * Method to check if the neighboring countries exist for a country
+	 * 
+	 * @param territories
+	 *            a hashMap of territories
+	 * @return false if neighboring countries do not exist
+	 * @return true if neighboring countries exist
+	 */
 	public boolean checkNeighbouringCountriesExist(HashMap<String, Territory> territories) {
 		for (Territory t : territories.values()) {
 			if (t.getNeighbouringTerritories().isEmpty()) {
@@ -56,32 +68,41 @@ public class ValidateMapReader {
 		}
 		return true;
 	}
-/**
- * Method to check if neighboring countries are symmetric
- * @param territories a hashMap of territory
- * @return false if neighboring countries are not symmetric
- * @return true if neighboring countries are symmetric
- */
+
+	/**
+	 * Method to check if neighboring countries are symmetric i.e. if A is neighbor
+	 * of a B then B is a neighbor of A.
+	 * 
+	 * @param territories
+	 *            a hashMap of territory
+	 * @return false if neighboring countries are not symmetric
+	 * @return true if neighboring countries are symmetric
+	 */
 	public boolean checkNeighbouringCountriesSymmetric(HashMap<String, Territory> territories) {
 		for (Territory t : territories.values()) {
 			if (!t.getNeighbouringTerritories().isEmpty()) {
 				for (String neighbouringTerritoryName : t.getNeighbouringTerritories()) {
-					if(!checkCountryExistInNeighbouringCountry(t.getName(), neighbouringTerritoryName, territories)) {
-						return false; 
+					if (!checkCountryExistInNeighbouringCountry(t.getName(), neighbouringTerritoryName, territories)) {
+						return false;
 					}
 				}
 			}
 		}
 		return true;
 	}
-/**
- * Method to check if country exist in neighboring country
- * @param mainCountry the main country whose neighbors have to be checked
- * @param neighbouringTerritoryName the names of the neighboring countries
- * @param territories a hashMap of territories
- * @return false if country does not exist in neighboring countries
- * @return true if country exist in neighboring countries
- */
+
+	/**
+	 * Method to check if country exists as neighboring country for the country
+	 * 
+	 * @param mainCountry
+	 *            the main country whose neighbors have to be checked
+	 * @param neighbouringTerritoryName
+	 *            the names of the neighboring countries
+	 * @param territories
+	 *            a hashMap of territories
+	 * @return false if country does not exist in neighboring countries
+	 * @return true if country exist in neighboring countries
+	 */
 	private boolean checkCountryExistInNeighbouringCountry(String mainCountry, String neighbouringTerritoryName,
 			HashMap<String, Territory> territories) {
 		for (Territory t : territories.values()) {
