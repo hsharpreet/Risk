b839896f232cd1548b280de4627ad77ec6501cad
sjbachher <sjbachher12@gmail.com>
2017-10-19 20:24:01 -0400

    Changes to MapWriterTest

--- RiskGame/src/game/risk/test/util/MapWriterTest.java
+++ RiskGame/src/game/risk/test/util/MapWriterTest.java
@@ -9,28 +9,44 @@ import game.risk.util.MapReader;
 import game.risk.util.MapWriter;
 import game.risk.util.RiskMap;
 
+/**
+ * Class to test all the methods of MapWriter
+ * 
+ * @author Team
+ *
+ */
 public class MapWriterTest {
-	
-	private String name =null;
+
+	private String name = null;
 	private String path = null;
 	private String value = null;
+
 	@Before
 	public void setUp() throws Exception {
-		 name = "new continet";
-		 path = "World.map";
-		 value = "5";
+		name = "new continet";
+		path = "World.map";
+		value = "5";
 	}
 
+	/**
+	 * Method to test whether the continent is added in the file
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testCheckContinentIsAdded() throws Exception {
 		MapWriter writer = new MapWriter(path);
 		writer.addContinent(name, value);
-		
+
 		assertTrue(name, new MapReader().readMap(path).getContinents().containsKey(name));
 
 	}
-	
-	
+
+	/**
+	 * Method to test whether continent is added in the file.
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testCheckContinentIsDeleted() throws Exception {
 		MapWriter writer = new MapWriter(path);
@@ -39,6 +55,5 @@ public class MapWriterTest {
 		assertFalse(name, new MapReader().readMap(path).getContinents().containsKey(name));
 
 	}
-	
 
 }
