d42e35543f69e990f9a56c298f7bf19094af11f3
Harpreet Singh <hs.harpreet101@gmail.com>
2017-11-03 14:28:22 -0400

    Moving Risk Game Constants to util package

---
+++ RiskGame/src/game/risk/util/RiskGameConstants.java
@@ -0,0 +1,20 @@
+package game.risk.util;
+
+import java.awt.Color;
+
+public class RiskGameConstants {
+
+	public static final String SELECT_MAP = "Select a Map below:";
+	public static final String OK = "Ok";
+	public static final String CANCEL = "Cancel";
+	public static final String TERRITORY = "";
+	public static final String CONTINENTS = "";
+	public static final String MAP = "";
+	public static final String GAME_NAME = "Risk ";
+	public static final String NO_OF_COMPUTERS = "Select no. of computers?";
+
+	public static final String FOLDER_FOR_MAPS_PATH = "Maps";
+	public static Color[] colorArray = {Color.RED, Color.DARK_GRAY, 
+			Color.BLUE, Color.GREEN, Color.MAGENTA};
+	
+}
