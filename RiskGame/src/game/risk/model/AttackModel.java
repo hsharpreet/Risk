4d94bc55bc58cfe8d05fb05f09b0001a0c54500f
himanshumahajan <himanshumahajan@192.168.2.27>
2017-11-03 16:55:00 -0400

    add else in my index

--- RiskGame/src/game/risk/model/AttackModel.java
+++ RiskGame/src/game/risk/model/AttackModel.java
@@ -153,6 +153,10 @@ public class AttackModel
                        {
                            tempList.get(i).isOwn = true;
                        }
+                       else 
+                       {
+                    	   
+                       }
                        break;
                    }
                }
