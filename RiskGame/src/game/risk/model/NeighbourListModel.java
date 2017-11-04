fdda6e7fbefeb6c91030ed84df385533ffabbe7f
himanshumahajan <himanshumahajan@192.168.2.27>
2017-11-04 14:12:11 -0400

    Making neighbour list model

---
+++ RiskGame/src/game/risk/model/NeighbourListModel.java
@@ -0,0 +1,21 @@
+package game.risk.model;
+
+import java.util.List;
+import javax.swing.AbstractListModel;
+
+/**
+ * This class is a list model which is used to make the displaying list dynamic
+ * 
+ */
+public class NeighbourListModel extends AbstractListModel
+{
+    public List<String> neighbours;
+
+    public NeighbourListModel(List<String> neighbours)
+    {
+        this.neighbours = neighbours;
+    }
+
+    
+    
+}
