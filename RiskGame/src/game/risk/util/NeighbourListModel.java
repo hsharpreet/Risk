05aad93ba35cdb7f5e180b2f173b90e6a4a3db29
Mandeep1151 <mandeep1151@gmail.com>
2017-11-08 14:28:05 -0500

    Deleting unwanted class and formatting NeighbourLiastModel Class

--- RiskGame/src/game/risk/util/NeighbourListModel.java
+++ RiskGame/src/game/risk/util/NeighbourListModel.java
@@ -7,27 +7,25 @@ import javax.swing.AbstractListModel;
  * This class is a list model which is used to make the displaying list dynamic
  * 
  */
-public class NeighbourListModel extends AbstractListModel
-{
-    public List<String> neighbours;
+public class NeighbourListModel extends AbstractListModel {
+	public List<String> neighbours;
 
-    public NeighbourListModel(List<String> neighbours)
-    {
-        this.neighbours = neighbours;
-    }
+	/**
+	 * 
+	 * @param neighbours
+	 */
+	public NeighbourListModel(List<String> neighbours) {
+		this.neighbours = neighbours;
+	}
 
-    @Override
-    public int getSize()
-    {
-        return neighbours.size();
-    }
+	@Override
+	public int getSize() {
+		return neighbours.size();
+	}
 
-    @Override
-    public Object getElementAt(int index)
-    {
-        return neighbours.get(index);
-    }
+	@Override
+	public Object getElementAt(int index) {
+		return neighbours.get(index);
+	}
 
-    
-    
 }
