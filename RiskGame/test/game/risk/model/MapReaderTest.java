c2c5a99dfe22473bdbc25c65d882bb1bba5ab6ca
Mandeep1151 <mandeep1151@gmail.com>
2017-11-07 23:58:25 -0500

    Fix compilation issues

--- RiskGame/test/game/risk/model/MapReaderTest.java
+++ RiskGame/test/game/risk/model/MapReaderTest.java
@@ -9,8 +9,7 @@ import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
-import game.risk.model.MapReader;
-import game.risk.model.RiskMap;
+import game.risk.util.MapReader;
 
 /**
  * Class to test MapReader
