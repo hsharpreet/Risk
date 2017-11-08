d821dffac87a1cfbad3633104e46d872e4f172ae
sjbachher <sjbachher12@gmail.com>
2017-11-08 18:41:57 -0500

    Refactoring of CustomLogRecord and loggerUtility

--- RiskGame/src/game/risk/util/CustomLogRecord.java
+++ RiskGame/src/game/risk/util/CustomLogRecord.java
@@ -4,15 +4,19 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 /**
+ * Class to keep the custom record
  * 
  * @author Team
  *
  */
 public class CustomLogRecord extends LogRecord {
 	/**
+	 * a constructor
 	 * 
 	 * @param level
+	 *            attribute of logRecord class
 	 * @param msg
+	 *            attribute of logRecord class
 	 */
 	public CustomLogRecord(Level level, String msg) {
 		super(level, msg);
--- RiskGame/src/game/risk/util/LoggerUtility.java
+++ RiskGame/src/game/risk/util/LoggerUtility.java
@@ -1,7 +1,11 @@
 package game.risk.util;
 
 import java.util.logging.ConsoleHandler;
-
+/**
+ * Class to create ConsoleHandler
+ * @author Team
+ *
+ */
 public class LoggerUtility
 {
     public static ConsoleHandler consoleHandler = new ConsoleHandler();
