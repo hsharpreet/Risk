7f1522f389fa276750fc8c99d2f4bd2b2c3e4dec
sjbachher <sjbachher12@gmail.com>
2017-11-08 18:34:24 -0500

    changes to CurrentGameStaticsTableModel

--- RiskGame/src/game/risk/util/CurrentGameStaticsTableModel.java
+++ RiskGame/src/game/risk/util/CurrentGameStaticsTableModel.java
@@ -7,9 +7,9 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 /**
- * @author Team
- * 
  * This class saves the current statics of the player
+ * 
+ * @author Team
  * 
  */
 public class CurrentGameStaticsTableModel extends AbstractTableModel {
@@ -19,25 +19,40 @@ public class CurrentGameStaticsTableModel extends AbstractTableModel {
 	String columnNames[] = { "Territory", "Continent", "Infantries" };
 
 	/**
+	 * a constructor that initializes the arrayList
 	 * 
 	 * @param list
+	 *            the arrayList
 	 */
 	public CurrentGameStaticsTableModel(List<CurrentGameStatics> list) {
 		this.list = list;
 	}
 
 	/**
+	 * Method to get the columnName
+	 * 
 	 * @param index
+	 *            an integer variable
 	 */
 	public String getColumnName(int index) {
 		return columnNames[index];
 	}
 
+	/**
+	 * Method to get the row count
+	 * 
+	 * @return the size of the list
+	 */
 	@Override
 	public int getRowCount() {
 		return list.size();
 	}
 
+	/**
+	 * Method to get the column count
+	 * 
+	 * @return the length of the columnname
+	 */
 	@Override
 	public int getColumnCount() {
 		return columnNames.length;
@@ -45,8 +60,13 @@ public class CurrentGameStaticsTableModel extends AbstractTableModel {
 
 	@Override
 	/**
+	 * Method to get the value of the territory continent and infantries at the
+	 * current index
+	 * 
 	 * @param rowIndex
+	 *            the row index
 	 * @param columnIndex
+	 *            the column index
 	 */
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		if (columnIndex == 0) {
