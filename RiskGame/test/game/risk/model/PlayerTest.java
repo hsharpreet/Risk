0536771485afc519abdc39f5f69bfdd6df43e727
Mandeep1151 <mandeep1151@gmail.com>
2017-11-08 23:27:00 -0500

    New Reinforcement test

--- RiskGame/test/game/risk/model/PlayerTest.java
+++ RiskGame/test/game/risk/model/PlayerTest.java
@@ -5,18 +5,19 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import game.risk.gui.RiskGame;
 import game.risk.util.CurrentGameStatics;
 import game.risk.util.MapReader;
 import game.risk.util.Territory;
 
 public class PlayerTest {
-
-	@Test
-	public void testReinforcementArmies() {
-		
+	
+	Player players[];
+	
+	@Before
+	public void setUpData(){
 		RiskMap mapDetails = MapReader.readMapFile("World_testingReinforcement.map");
 		Territory t1= new Territory();
 		t1.setName("test1");
@@ -38,15 +39,29 @@ public class PlayerTest {
 		CurrentGameStatics gamestat2 = new CurrentGameStatics(2, t2);
 		CurrentGameStatics gamestat3 = new CurrentGameStatics(2, t3);
 		CurrentGameStatics gamestat4 = new CurrentGameStatics(2, t4);
-		Player players[] = new Player[1];
-		players[0]= new Player(new RiskGame(), 1, players, mapDetails);
+		players = new Player[1];
+		players[0]= new Player(1, players, mapDetails);
 		players[0].currentGameStaticsList= new ArrayList<>();
 		players[0].currentGameStaticsList.add(gamestat1);
 		players[0].currentGameStaticsList.add(gamestat2);
 		players[0].currentGameStaticsList.add(gamestat3);
 		players[0].currentGameStaticsList.add(gamestat4);
+	}
 
+	
+	@Test
+	public void testReinforcementArmies_UnoccupiedContinent() {
 		assertEquals(3, players[0].calculateReinformentArmies(0));
+	}
+	
+	@Test
+	public void testReinforcementArmies_OccupiedContinent() {
+		Territory t5= new Territory();
+		t5.setName("test3");
+		t5.setContinent("hello");
+		CurrentGameStatics gamestat5 = new CurrentGameStatics(2, t5);
+		players[0].currentGameStaticsList.add(gamestat5);
+		assertEquals(4, players[0].calculateReinformentArmies(0));
 	}
 
 }
