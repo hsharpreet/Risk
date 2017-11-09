8f6575df58f23f8eae4fa43fa549ca6d693f2140
sjbachher <sjbachher12@gmail.com>
2017-11-08 20:40:29 -0500

    refactoring of ReinforcementClickListener.java

--- RiskGame/src/game/risk/listener/ReinforcementClickListener.java
+++ RiskGame/src/game/risk/listener/ReinforcementClickListener.java
@@ -13,50 +13,64 @@ import java.util.logging.Level;
 import game.risk.util.CustomLogRecord;
 import javax.swing.JOptionPane;
 
-public class ReinforcementClickListener implements ActionListener
-    {
+/**
+ * method to create a listener for reinforcement which places the infantry in
+ * the territory
+ * 
+ * @author Team
+ *
+ */
+public class ReinforcementClickListener implements ActionListener {
 
-        int i;
-        Player player[];
+	int i;
+	Player player[];
 
-        public ReinforcementClickListener(int i, Player player[])
-        {
-            this.i = i;
-            this.player = player;
-        }
+	/**
+	 * a constructor
+	 * 
+	 * @param i
+	 *            an integer value
+	 * @param player
+	 *            an array of player class
+	 */
+	public ReinforcementClickListener(int i, Player player[]) {
+		this.i = i;
+		this.player = player;
+	}
 
-        public void actionPerformed(ActionEvent e)
-        {
-            if (player[i].infantriesAvailable > 0)
-            {
-                int index = player[i].getPlayerPanel().jtCountriesAndArmies.getSelectedRow();
-                if (index == -1)
-                {
-                    JOptionPane.showMessageDialog(player[i].getPlayerPanel(), "Select terriotary first");
-                } else
-                {
-                    player[i].currentGameStaticsList.get(index).infantries++;
-                    player[i].infantriesAvailable--;
-                    player[i].getPlayerPanel().lbAvailableArmies.setText("Available Infantries : " + player[i].infantriesAvailable);
-                    player[i].currentGameStaticsTableModel.fireTableDataChanged();
+	/**
+	 * Method to perform the action of reinforcement
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (player[i].infantriesAvailable > 0) {
+			int index = player[i].getPlayerPanel().jtCountriesAndArmies.getSelectedRow();
+			if (index == -1) {
+				JOptionPane.showMessageDialog(player[i].getPlayerPanel(), "Select terriotary first");
+			} else {
+				player[i].currentGameStaticsList.get(index).infantries++;
+				player[i].infantriesAvailable--;
+				player[i].getPlayerPanel().lbAvailableArmies
+						.setText("Available Infantries : " + player[i].infantriesAvailable);
+				player[i].currentGameStaticsTableModel.fireTableDataChanged();
 
-                    player[i].setMessage("Reinforcement Phase\r\nPlayer - "+(i+1)+" has placed infantry in "+player[i].currentGameStaticsList.get(index).territory.getName().toUpperCase());
-                    player[i].notifyObservers();
-                    
-                    CustomLogRecord logRecord = new CustomLogRecord(Level.INFO, "Player - "+(i+1)+" has placed infantry in "+player[i].currentGameStaticsList.get(index).territory.getName().toUpperCase());
-                    LoggerUtility.consoleHandler.publish(logRecord);
-                    
-                    if (player[i].infantriesAvailable == 0)
-                    {
-                        player[i].getPlayerPanel().btReinforcement.setEnabled(false);
-                        player[i].attack(i);
-                    }
+				player[i].setMessage("Reinforcement Phase\r\nPlayer - " + (i + 1) + " has placed infantry in "
+						+ player[i].currentGameStaticsList.get(index).territory.getName().toUpperCase());
+				player[i].notifyObservers();
 
-                }
-            } else
-            {
-                JOptionPane.showMessageDialog(player[i].getPlayerPanel(), "No army available");
+				CustomLogRecord logRecord = new CustomLogRecord(Level.INFO,
+						"Player - " + (i + 1) + " has placed infantry in "
+								+ player[i].currentGameStaticsList.get(index).territory.getName().toUpperCase());
+				LoggerUtility.consoleHandler.publish(logRecord);
 
-            }
-        }
-    }
+				if (player[i].infantriesAvailable == 0) {
+					player[i].getPlayerPanel().btReinforcement.setEnabled(false);
+					player[i].attack(i);
+				}
+
+			}
+		} else {
+			JOptionPane.showMessageDialog(player[i].getPlayerPanel(), "No army available");
+
+		}
+	}
+}
