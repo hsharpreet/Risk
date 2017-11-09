21d747daf26d8aac5923bec35062bd5b849433bc
sjbachher <sjbachher12@gmail.com>
2017-11-08 20:14:28 -0500

    refactoring of MyListSelectionListener

--- RiskGame/src/game/risk/listener/MyListSelectionListener.java
+++ RiskGame/src/game/risk/listener/MyListSelectionListener.java
@@ -9,31 +9,44 @@ import game.risk.model.Player;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-public class MyListSelectionListener implements ListSelectionListener
-    {
+/**
+ * A class to create a listener for creating a list of the player details
+ * 
+ * @author Team
+ *
+ */
+public class MyListSelectionListener implements ListSelectionListener {
 
-        int i;
-        Player player[];
+	int i;
+	Player player[];
 
-        public MyListSelectionListener(int i,Player player[])
-        {
-            this.i = i;
-            this.player = player;
-        }
+	/**
+	 * A constructor
+	 * 
+	 * @param i
+	 *            an integer value
+	 * @param player
+	 *            an array of Player class
+	 */
+	public MyListSelectionListener(int i, Player player[]) {
+		this.i = i;
+		this.player = player;
+	}
 
-        @Override
-        public void valueChanged(ListSelectionEvent e)
-        {
-            if (e.getValueIsAdjusting())
-            {
-                int index = player[i].getPlayerPanel().jtCountriesAndArmies.getSelectedRow();
-                if (index >= 0)
-                {
-                    player[i].neighbourListModel.neighbours = player[i].currentGameStaticsList.get(index).territory.getNeighbouringTerritories();
-                    player[i].getPlayerPanel().lsNeighbour.updateUI();
+	@Override
+	/**
+	 * Method which is called whenever a selection is changed
+	 */
+	public void valueChanged(ListSelectionEvent e) {
+		if (e.getValueIsAdjusting()) {
+			int index = player[i].getPlayerPanel().jtCountriesAndArmies.getSelectedRow();
+			if (index >= 0) {
+				player[i].neighbourListModel.neighbours = player[i].currentGameStaticsList.get(index).territory
+						.getNeighbouringTerritories();
+				player[i].getPlayerPanel().lsNeighbour.updateUI();
 
-                }
-            }
-        }
+			}
+		}
+	}
 
-    }
+}
