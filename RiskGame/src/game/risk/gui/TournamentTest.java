f2909e400ab7276dfb96234c861faf9803535998
Mandeep1151 <mandeep1151@gmail.com>
2017-11-30 17:54:56 -0500

    Changing Frame Settings of Tournamnet

--- RiskGame/src/game/risk/gui/TournamentTest.java
+++ RiskGame/src/game/risk/gui/TournamentTest.java
@@ -68,7 +68,7 @@ public class TournamentTest extends javax.swing.JFrame {
 		jScrollPane1 = new javax.swing.JScrollPane();
 		btPlayTournament = new javax.swing.JButton();
 
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 		setTitle("Tournament");
 
 		lbPlayer1.setText("Player 1");
