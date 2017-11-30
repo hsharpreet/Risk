9931ddb5b6f54ec1a55f10f54e3a841592650cab
amanp <amanp@WINDOWS-FFEVLUO.local>
2017-11-30 03:38:44 -0500

    aggressive player fortification

--- RiskGame/src/game/risk/model/entities/strategy/AggressivePlayerStrategy.java
+++ RiskGame/src/game/risk/model/entities/strategy/AggressivePlayerStrategy.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 
@@ -136,109 +137,59 @@ public class AggressivePlayerStrategy  implements PlayerStrategy, Serializable  
 	}
 
 	@Override
-	public int fortificationStrategy(int i, Player random, int army) {
-
-		ArrayList<String> terrList = new ArrayList<String>();
-		ArrayList<String> list = new ArrayList<String>();
-
-		ArrayList<String> maxArmy = new ArrayList<String>();
-		ArrayList<String> minArmy = new ArrayList<String>();
-		
-		for (int j = 0; j < random.currentGameStaticsList.size(); j++) {
-			list.add(random.currentGameStaticsList.get(j).territory.getName());
+	public int fortificationStrategy(int i, Player aggresivePlayer, int army) {
+		List<String> playerTerritoriesNames = new ArrayList<>();
+		for (int j = 0; j < aggresivePlayer.currentGameStaticsList.size(); j++) {
+			playerTerritoriesNames.add(aggresivePlayer.currentGameStaticsList.get(j).territory.getName());
 		}
-
-		for (int j = 0; j < list.size(); j++) {
-			for (int jj = 0; jj < random.currentGameStaticsList.get(j).territory.getNeighbouringTerritories()
-					.size(); jj++) {
-				String destinationTerritory = random.currentGameStaticsList.get(j).territory
-						.getNeighbouringTerritories().get(jj);
-
-				if (list.contains(destinationTerritory) && random.currentGameStaticsList.get(j).infantries > 6) {
-					maxArmy.add(j + ":" + destinationTerritory);
-				}
-				if(maxArmy.size() >= 5){
-					break;
-				}
-			}
-			if(maxArmy.size() < 5){
-				for (int jj = 0; jj < random.currentGameStaticsList.get(j).territory.getNeighbouringTerritories()
-						.size(); jj++) {
-					String destinationTerritory = random.currentGameStaticsList.get(j).territory
-							.getNeighbouringTerritories().get(jj);
-
-					if (list.contains(destinationTerritory) && random.currentGameStaticsList.get(j).infantries > 4) {
-						maxArmy.add(j + ":" + destinationTerritory);
-					}
-					if(maxArmy.size() >= 5){
-						break;
-					}
+		boolean flag = true;
+		// Array of territories which have highest army but dont get fortified
+		List<String> highestArmyTerritories = new ArrayList<>();
+		while (flag) {
+			int maxArmy = 0;
+			int maxArmyIndex = -1;
+			for (int j = 0; j < aggresivePlayer.currentGameStaticsList.size(); j++) {
+				if (aggresivePlayer.currentGameStaticsList.get(j).infantries > maxArmy && !highestArmyTerritories
+						.contains(aggresivePlayer.currentGameStaticsList.get(j).territory.getName())) {
+					maxArmy = aggresivePlayer.currentGameStaticsList.get(j).infantries;
+					maxArmyIndex = j;
 				}
 			}
-			
-			if(maxArmy.size() < 5){
-				for (int jj = 0; jj < random.currentGameStaticsList.get(j).territory.getNeighbouringTerritories()
-						.size(); jj++) {
-					String destinationTerritory = random.currentGameStaticsList.get(j).territory
-							.getNeighbouringTerritories().get(jj);
-
-					if (list.contains(destinationTerritory) && random.currentGameStaticsList.get(j).infantries > 2) {
-						maxArmy.add(j + ":" + destinationTerritory);
-					}
-					if(maxArmy.size() >= 5){
-						break;
+			int count = 0;
+			if (maxArmyIndex == -1) {
+				aggresivePlayer.setMessage("Fortification Phase\r\nPlayer - " + aggresivePlayer.getName()
+						+ " cant fortify as it has no country which can move its armies to neighbours  ");
+				aggresivePlayer.notifyObservers();
+				return 0;
+			} else {
+				for (String neighbour : aggresivePlayer.currentGameStaticsList.get(maxArmyIndex).territory
+						.getNeighbouringTerritories()) {
+					if (playerTerritoriesNames.contains(neighbour)) {
+						int index = playerTerritoriesNames.indexOf(neighbour);
+						if (aggresivePlayer.currentGameStaticsList.get(index).infantries > 1) {
+							count++;
+							aggresivePlayer.currentGameStaticsList.get(index).infantries--;
+							aggresivePlayer.currentGameStaticsList.get(maxArmyIndex).infantries++;
+							aggresivePlayer.setMessage("Fortification Phase\r\nPlayer - " + aggresivePlayer.getName()
+									+ " has transfered 1 infantry from "
+									+ aggresivePlayer.currentGameStaticsList.get(index).territory.getName() + " to "
+									+ aggresivePlayer.currentGameStaticsList.get(maxArmyIndex).territory.getName());
+							aggresivePlayer.notifyObservers();
+						}
 					}
 				}
-			}
-		}
-		
-		for (int j = 0; j < list.size(); j++) {
-			if (random.currentGameStaticsList.get(j).infantries  == 0) {
-				minArmy.add(j+"");
-			}
-		}
-		while(minArmy.size() <=5){
-			for (int j = 0; j < list.size(); j++) {
-				if (random.currentGameStaticsList.get(j).infantries  <= 2) {
-					minArmy.add(j+"");
+				if (count > 0) {
+					flag = false;
+
+				} else if (count == 0) {
+					highestArmyTerritories
+							.add(aggresivePlayer.currentGameStaticsList.get(maxArmyIndex).territory.getName());
 				}
 			}
 		}
-		
-		for(int j = 0; j < minArmy.size(); j++){
-			//TODO
-		}
-		
-		for (int j = 0; j < list.size(); j++) {
-			for (int jj = 0; jj < random.currentGameStaticsList.get(j).territory.getNeighbouringTerritories()
-					.size(); jj++) {
-				String destinationTerritory = random.currentGameStaticsList.get(j).territory
-						.getNeighbouringTerritories().get(jj);
-
-				if (list.contains(destinationTerritory) && random.currentGameStaticsList.get(j).infantries > 1) {
-					terrList.add(j + ":" + destinationTerritory);
-				}
-			}
-		}
-		int possibleMoves = terrList.size();
-		int randomMoves = new Random().nextInt(possibleMoves);
-
-		for (int k = 0; k < randomMoves; k++) {
-			int minus = Integer.parseInt(terrList.get(k).split(":")[0]);
-
-			String destinationTerritory = terrList.get(k).split(":")[1];
-			int plus = list.indexOf(destinationTerritory);
-
-			random.currentGameStaticsList.get(minus).infantries--;
-			random.currentGameStaticsList.get(plus).infantries++;
-
-			random.setMessage("Fortification Phase\r\nPlayer - " + random.getName() + " has transfered 1 infantry from "
-					+ random.currentGameStaticsList.get(minus).territory.getName() + " to " + destinationTerritory);
-			random.notifyObservers();
-
-			random.currentGameStaticsTableModel.fireTableDataChanged();
-		}
-
+		aggresivePlayer.currentGameStaticsTableModel.fireTableDataChanged();
+		aggresivePlayer.setMessage("Player - " + aggresivePlayer.getName() + " fortification phase ended  ");
+		aggresivePlayer.notifyObservers();
 		return 0;
 	}
 	
