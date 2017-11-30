bc9af3abb3edf7210dfa3243f58e9b8fd5b05788
sjbachher <sjbachher12@gmail.com>
2017-11-30 16:43:12 -0500

    javadoc added to PlayerStartegy

--- RiskGame/src/game/risk/model/entities/strategy/PlayerStrategy.java
+++ RiskGame/src/game/risk/model/entities/strategy/PlayerStrategy.java
@@ -7,21 +7,32 @@ import game.risk.model.entities.RiskMap;
 import game.risk.util.CustomLogRecord;
 import game.risk.util.LoggerUtility;
 
+/**
+ * Class to create interface for Player Strategy
+ * 
+ * @author Team
+ *
+ */
 public interface PlayerStrategy {
 
 	public int placeInfantoryStrategy(int i, Player player, int army);
-	
+
 	public int reinforcementStrategy(int i, Player player, int army);
 
-	public int attackStrategy(Player player[],int i, Player player2, RiskMap mapDetails);
+	public int attackStrategy(Player player[], int i, Player player2, RiskMap mapDetails);
 
 	public int fortificationStrategy(int i, Player player, int army);
-	
+
+	/**
+	 * Method to generate a random number
+	 * 
+	 * @param size
+	 * @return a random number
+	 */
 	public default int randomNumber(int size) {
 		double d = Math.random();
 		int random = (int) Math.round(d * (size - 1));
-		CustomLogRecord logRecord = new CustomLogRecord(Level.INFO,
-				"Random number generated: ");
+		CustomLogRecord logRecord = new CustomLogRecord(Level.INFO, "Random number generated: ");
 		LoggerUtility.consoleHandler.publish(logRecord);
 		return random;
 	}
