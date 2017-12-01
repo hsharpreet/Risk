476d6419de5a2096aa9d3a67e19611ce057438f7
sjbachher <sjbachher12@gmail.com>
2017-11-30 20:02:52 -0500

    javadoc added to tournamentresult

--- RiskGame/src/game/risk/model/entities/TournamentResult.java
+++ RiskGame/src/game/risk/model/entities/TournamentResult.java
@@ -1,27 +1,71 @@
 package game.risk.model.entities;
 
+/**
+ * Class to generate Tournament result
+ * 
+ * @author Team
+ *
+ */
 public class TournamentResult {
 	private String mapName;
-    private int gameIndex;
-    private String winnerName;
-    
+	private int gameIndex;
+	private String winnerName;
+
+	/**
+	 * Method to get Map Name
+	 * 
+	 * @return the name of map
+	 */
 	public String getMapName() {
 		return mapName;
 	}
+
+	/**
+	 * Method to set the attribute value
+	 * 
+	 * @param mapName
+	 *            the value
+	 */
 	public void setMapName(String mapName) {
 		this.mapName = mapName;
 	}
+
+	/**
+	 * Method to get Game index
+	 * 
+	 * @return the index number
+	 */
 	public int getGameIndex() {
 		return gameIndex;
 	}
+
+	/**
+	 * Method to set the index value in class attribute
+	 * 
+	 * @param gameIndex
+	 *            the index number
+	 */
 	public void setGameIndex(int gameIndex) {
 		this.gameIndex = gameIndex;
 	}
+
+	/**
+	 * Method to get the name of the winner
+	 * 
+	 * @return the winner name
+	 */
 	public String getWinnerName() {
 		return winnerName;
 	}
+
+	/**
+	 * Method to set the name of the winner in class attribute
+	 * 
+	 * @param winnerName
+	 *            the name of the winner
+	 */
 	public void setWinnerName(String winnerName) {
 		this.winnerName = winnerName;
-	}    
+	}
 
 }
